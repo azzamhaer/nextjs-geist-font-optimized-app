1803deb90f265f83488959b8580d96c51b138aa5
Azzam Hudiya <117409466+azzamhaer@users.noreply.github.com>
2025-07-29 14:36:02 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1753774560498

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@supabase_node-fetch_browser_de85a9f1.js",
+  "static/chunks/node_modules_0900853e._.js",
+  "static/chunks/src_30a0e14b._.js",
+  "static/chunks/src_app_globals_b805903d.css"
+],
+    source: "dynamic"
+});
